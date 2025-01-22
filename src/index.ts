d0229871f8726b45b564199a8ceabe2e8c4e2913
SSNoah <emmanuelalfonsociencia@gmail.com>
2025-01-22 20:55:41 +0800

    initialized tsx, pkgroll and vscode debugger basic configs

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("Hello world!");
+// console.log(process.env.DB_HOST);
+const testAdd = (first: number, second: number): number => {
+  const sum = first + second;
+  return sum;
+};
+
+testAdd(50, 43);
