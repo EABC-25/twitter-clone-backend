6fdfa458ce8dd96d11c49686e9ebe5cfb90d9767
SSNoah <emmanuelalfonsociencia@gmail.com>
2025-05-25 21:54:11 +0800

    explicitly adding port connection in mysql pool access options

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,6 +8,7 @@ const access: PoolOptions = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 20,
   queueLimit: 0,
